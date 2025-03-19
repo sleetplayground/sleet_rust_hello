2eb79ab57bf7120146c4cadf70810008c9cf7327
The SunShining <thesunshiningmin@gmail.com>
2025-03-19 00:08:23 -0600

    Move tests to separate unit_tests.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -31,25 +31,7 @@ impl Contract {
     }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
+///👋
+/// TESTS !!!
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(contract.get_greeting(), "Hello");
-    }
-
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(contract.get_greeting(), "howdy");
-    }
-}
+mod unit_tests;
